d8d2dbad5b13cec9c1fb87695e3c0bf012400e63
ericu <ericu@ubuntu-lts-base.(none)>
2013-09-27 19:24:28 -0400

    woohoo, got it right!

--- probecap.sql
+++ probecap.sql
@@ -1,29 +1,33 @@
 
-CREATE TABLE station{
-	id serial not null,
+CREATE TABLE station(
+	id serial not null UNIQUE,
 	mac macaddr not null,
 	firstSeen timestamp without time zone not null,
 	lastSeen timestamp without time zone,
 	PRIMARY KEY(mac)
-};
+);
 
 grant usage,select on sequence station_id_seq to probecap;
 
-CREATE TABLE ssid{
-	id serial not null
-	name varchar(32) not null
-};
+CREATE TABLE ssid(
+	id serial not null UNIQUE,
+	name varchar(32) not null,
+	PRIMARY KEY(name)
+);
 
 grant usage,select on sequence ssid_id_seq to probecap;
 
-CREATE TABLE probe{
-	station int references station(id) not null,
-	ssid int references ssid(id) null,
+CREATE TABLE probe(
+	station int not null,
+	foreign key (station) references station(id),
+	ssid int null,
+	foreign key (ssid) references ssid(id) ,
 	seen timestamp without time zone
-};
+);
 
-CREATE TABLE beacon{
-	station int references station(id) not null,
+CREATE TABLE beacon(
+	station int not null,
+	foreign key(station) references station(id),
 	ssid int references ssid(id) null,
 	seen timestamp without time zone
-};
+);
